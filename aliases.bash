e22474ec9d877d899bb56d6e26b6a507c4ce175c
Alex Zabolotnyi <mylonov@gmail.com>
2021-02-06 13:09:15 +0100

    Changes

--- aliases.bash
+++ aliases.bash
@@ -1,1 +1,8 @@
 alias la="ls -la"
+alias vup="vagrant up"
+alias vdn="vagrant halt"
+
+alias portsup="sudo pfctl -ef /etc/pf.conf"
+alias portsdn="sudo pfctl -df /etc/pf.conf"
+
+alias pu="./vendor/bin/phpunit"
--- index.bash
+++ index.bash
@@ -5,6 +5,7 @@ FW_DIR="$HOME/.dotfiles"
 source $FW_DIR/git-completion.bash
 source $FW_DIR/git-aliases.bash
 source $FW_DIR/aliases.bash
+source $FW_DIR/paths.bash
 
 if [ -f $FW_DIR/local-overrides.bash ]; then
     source $FW_DIR/local-overrides.bash
